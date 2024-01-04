8240cd0e7585357c71ed5ec9e78045ee5c2a8d0b
MicaheDev <webdev.michell@gmail.com>
2024-01-04 10:40:37 -0300

    init: file uploader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileUploaderComponent } from './file-uploader/file-uploader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/file-uploader/file-uploader.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-uploader',
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.scss']
+})
+export class FileUploaderComponent {
+  selectedFile!: string
+
+
+  readInputFile($event: any){
+    console.log($event.target.files[0])
+    this.selectedFile = $event.target.files[0].name
+  }
+
+}
